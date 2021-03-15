5065ffe92a3eb73a17aea69724c61f30252087f9
Siodifport39459 <37133076+Siodifport39459@users.noreply.github.com>
2021-03-15 14:01:28 +0530

    ghgh
    bghfg

---
+++ App.js
@@ -0,0 +1,37 @@
+//import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import  LoginScreen from './src/screens/LoginScreen';
+import { HomeScreen} from './src/screens/HomeScreen';
+import  RegistrationScreen  from './src/screens/RegistrationScreen'
+import AboutScreen from './src/screens/About'
+/*import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }*/
+
+const Stack = createStackNavigator();
+
+ function App () {
+
+  /*const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)*/
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/*{ user ? (
+          <Stack.Screen name="Home">
+            {props => }
+          </Stack.Screen>
+        ) : (
+        <>*/}
+          <HomeScreen  />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+      };
+export default App;
